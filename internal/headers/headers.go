d30e04fb3b950a93e70f111d3af4fcd484185b18
aem-0 <backendguy_2.10.25@proton.me>
2026-06-14 19:07:21 +0330

    feat(headers): http headers

---
+++ internal/headers/headers.go
@@ -0,0 +1,105 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if ch >= 'A' && ch <= 'Z' ||
+			ch >= 'a' && ch <= 'z' ||
+			ch >= '0' && ch <= '9' {
+			found = true
+		}
+		switch ch {
+		case '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+		}
+
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+var rn = []byte("/r/n")
+
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("bad field line")
+	}
+
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", fmt.Errorf("bad field name")
+	}
+
+	return string(name), string(value), nil
+}
+
+type Headers struct {
+	headers map[string]string
+}
+
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
+}
+
+func (h *Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h *Headers) Set(name, value string) {
+	name = strings.ToLower(name)
+	if v, ok := h.headers[name]; ok {
+		h.headers[name] = fmt.Sprintf("%s,%s", v, value)
+	} else {
+		h.headers[strings.ToLower(name)] = value
+	}
+}
+
+func (h *Headers) ForEach(cb func(n, v string)) {
+	for n, v := range h.headers {
+		cb(n, v)
+	}
+}
+
+func (h *Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+
+	for {
+		idx := bytes.Index(data[read:], rn)
+		fmt.Printf("parsing header (%d) - %d\n", read, idx)
+		if idx == -1 {
+			break
+		}
+
+		if idx == 0 {
+			done = true
+			read += len(rn)
+			break
+		}
+		fmt.Printf("header: \"%s\"\n", string(data[read:idx]))
+		name, value, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return 0, false, err
+		}
+
+		if !isToken([]byte(name)) {
+			return 0, false, fmt.Errorf("bad header name")
+		}
+
+		read += idx + len(rn)
+		h.Set(name, value)
+	}
+	return read, done, nil
+}
